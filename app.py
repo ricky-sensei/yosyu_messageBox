d953e1c447f3de01130ca27fc1c40af89bd4dbea
ricky-sensei <axcelcamp.teacher@gmail.com>
2024-12-12 02:34:21 +0900

    ログイン機能終了

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from config import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
@@ -12,7 +12,9 @@ app.secret_key = "secret"
 
 # ログインマネージャーをインスタンス化
 # ユーザーのログイン状態を管理してくれる便利なやつ
+# login_managerのインスタンス化と初期化
 login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # ログイン中のユーザー情報を取得するためのメソッド
@@ -44,19 +46,27 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    print("login")
+    if request.method =="POST":
+        if not request.form["password"] or not request.form["email"]:      
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+
+        user = User.select().where(User.email == request.form["email"]).first()
+        if user is not None and check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            
+            flash(f"ようこそ{user.name}さん")
+            return redirect("/")
+        flash("認証に失敗しました。")
+
     return render_template("login.html")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
 
 
 if __name__ == "__main__":
